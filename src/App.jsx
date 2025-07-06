d06deaa96d287162bf7c1189110ba0cd23edd2a4
Sanjay <sanjaymunda0123@gmail.com>
2025-07-06 23:26:26 +0530

    Menu Section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,10 +14,10 @@ const app = () => {
     <main>
       <Navbar />
       <Hero />
-      {/* <div className="h-dvh bg-black"></div> */} {/* temporary div */}
       <Cocktails />
       <About />
       <Art />
+      <Menu />
     </main>
   );
 };
